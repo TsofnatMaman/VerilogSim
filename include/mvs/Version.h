974a37988aa8c090e5b0441a779183a05cbf8b53
TsofnatMaman <tsofnat2971@gmail.com>
2025-08-29 00:18:33 +0300

    feat: add Version.h and print project version in main

---
+++ include/mvs/Version.h
@@ -0,0 +1,2 @@
+#pragma once
+#define MVS_VERSION "0.1.0"
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include "mvs/Version.h"
+
 int main() {
-    std::cout << "MiniVerilogSim starting..." << std::endl;
+    std::cout << "MiniVerilogSim starting..." << MVS_VERSION << std::endl;
     return 0;
 }
